f5f29fc2cdeba77c8056d091cdcb7cbe7d3dd291
Nibash-Chandan-Sahu <nibashsahu7@gmail.com>
2025-08-29 20:08:12 +0530

    feat(config ): add clerk image hostname to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
+
+
+
+
+
+git add next.config.ts
+git commit -m "feat(config ): add clerk image hostname to next.config.ts"
+git push
